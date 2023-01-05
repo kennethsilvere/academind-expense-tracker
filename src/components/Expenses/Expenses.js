4b44e5774f318a0dc154f71ab2cf32e4beb8e52c
Kenneth Silvere <kennethsilvere93@gmail.com>
2023-01-05 21:46:21 +0530

    React basics

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.list[0].title}
+        amount={props.list[0].amount}
+        date={props.list[0].date}
+      />
+      <ExpenseItem
+        title={props.list[1].title}
+        amount={props.list[1].amount}
+        date={props.list[1].date}
+      />
+      <ExpenseItem
+        title={props.list[2].title}
+        amount={props.list[2].amount}
+        date={props.list[2].date}
+      />
+      <ExpenseItem
+        title={props.list[3].title}
+        amount={props.list[3].amount}
+        date={props.list[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
